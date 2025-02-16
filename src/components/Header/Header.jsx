4c742d83e46d091f2e3ccf74933986813658362c
Manuel Torres <manueljosetorres02@gmail.com>
2025-02-16 12:03:11 -0600

    update nav section, need to work on Hover button behavior and clean up views

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import FilterIcon from "../../assets/images/Filter.svg";
-
+import FilterIcon from "../../assets/images/Filter.svg?react";
+import "./Header.css";
+// import "../styles/partials/button.css";
 const Header = () => {
   return (
     <>
-      <nav>
-        <h1>Snaps</h1>
+      <nav className="nav">
+        <h1 className="nav__header">Snaps</h1>
         {/* <button>Filters {FilterIcon}</button> */}
         {/* <img src="./src/assets/images/Filter.svg" /> */}
-        <img src={FilterIcon} />
+        <button className="nav__button">
+          Filter
+          {/* <img className="nav__icon" src={FilterIcon} /> */}
+          <FilterIcon className="nav__icon" />
+        </button>
       </nav>
     </>
   );
