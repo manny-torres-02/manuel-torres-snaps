d006d1a85e492c244f3889534999f9bb2030e42b
Manuel Torres <manueljosetorres02@gmail.com>
2025-02-17 08:32:25 -0600

    finsih out general header view

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
-
+import footer from "./"
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
