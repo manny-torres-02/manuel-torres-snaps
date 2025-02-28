100ff12bc05697371b07b46711d7e5d7d91a2683
Manuel Torres <manueljosetorres02@gmail.com>
2025-02-27 19:39:50 -0600

    complete testing and shifting api to gallery level

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,12 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import Card from "../Card/Card";
 import ImageData from "../../data/photos.json";
 import "./Gallery.scss";
 
 // let data = ImageData;
-const Gallery = ({ selectedTag, showFiltered, showTags }) => {
+const Gallery = ({ baseURL, selectedTag, showFiltered, showTags }) => {
+  const [photoData, setPhotoData] = useState();
   const test = ImageData.filter((image) =>
     selectedTag ? image.tags.includes(selectedTag) : true
   );
+  let data;
+
+  useEffect(() => {
+    //function to pull the key
+    // const fetchKey = async () => {
+    //   try {
+    //     const response = await axios.get(`${baseURL}register`);
+    //     console.log(response);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    const fetchPhotoData = async () => {
+      try {
+        const fetchData = await axios.get(
+          `${baseURL}photos/?api_key=71e72653-f4b0-4ace-9453-cd4c8c9a9ccf`
+        );
+        console.log("this is from the call", fetchData.data);
+        data = fetchData.data;
+        setPhotoData(fetchData.data);
+        console.log("running setPhotoData!: ", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }; // fetchKey();
+    // TODO: run if conditional to check if there is any type of photo data:
+
+    fetchPhotoData();
+  }, []);
+
+  console.log("running PhotioData outside of useEffect", photoData[1].tags);
 
   return (
     <>
@@ -22,7 +56,6 @@ const Gallery = ({ selectedTag, showFiltered, showTags }) => {
             timestamp={image.timestamp}
             tags={image.tags}
             comments={image.comments}
-            
           />
         ))}
       </div>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
-// import
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import Hero from "../../components/Hero/Hero";
 import FilterTags from "../../components/FilterTags/FilterTags";
@@ -7,15 +7,14 @@ import Gallery from "../../components/Gallery/Gallery";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
-import axios from "axios";
 const HomePage = () => {
   const [showTags, setShowTags] = useState(false);
   const [selectedTag, setSelectedTag] = useState(null);
-  const [photoData, setPhotoData] = useState();
+  //
 
   const baseURL = "https://unit-3-project-c5faaab51857.herokuapp.com/";
   const key = "3b75373b-9ad0-47af-bd5d-707946328244";
-  let data = "";
+
   function handleFilterClick() {
     setShowTags(!showTags);
 
@@ -24,36 +23,32 @@ const HomePage = () => {
     }
   }
 
-  useEffect(() => {
-    //function to pull the key
-    // const fetchKey = async () => {
-    //   try {
-    //     const response = await axios.get(`${baseURL}register`);
-    //     console.log(response);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    const fetchPhotoData = async () => {
-      try {
-        const fetchData = await axios.get(
-          `${baseURL}photos/?api_key=71e72653-f4b0-4ace-9453-cd4c8c9a9ccf`
-        );
-        console.log("this is from the call", fetchData.data);
-        let data = fetchData.data;
-        setPhotoData(fetchData.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  // useEffect(() => {
+  //   //function to pull the key
+  //   // const fetchKey = async () => {
+  //   //   try {
+  //   //     const response = await axios.get(`${baseURL}register`);
+  //   //     console.log(response);
+  //   //   } catch (error) {
+  //   //     console.log(error);
+  //   //   }
+  //   const fetchPhotoData = async () => {
+  //     try {
+  //       const fetchData = await axios.get(
+  //         `${baseURL}photos/?api_key=71e72653-f4b0-4ace-9453-cd4c8c9a9ccf`
+  //       );
+  //       console.log("this is from the call", fetchData.data);
+  //       data = fetchData.data;
+  //       setPhotoData(fetchData.data);
+  //       console.log("running setPhotoData!: ", data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
 
-    // fetchKey();
-    fetchPhotoData();
-    console.log("setphotodata:", photoData);
-  }, []);
-
-  useEffect(() => {
-    console.log("setPhotoData:", photoData);
-  }, [photoData]);
+  //   // fetchKey();
+  //   fetchPhotoData();
+  // }, []);
 
   return (
     <>
@@ -68,7 +63,11 @@ const HomePage = () => {
         />
         <div className="Hero-Gallery-wrapper">
           <Hero />
-          <Gallery selectedTag={selectedTag} showFiltered={showTags} />
+          <Gallery
+            baseURL={baseURL}
+            selectedTag={selectedTag}
+            showFiltered={showTags}
+          />
         </div>
       </div>
       <Footer />
